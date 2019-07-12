bc772c336baec65968efbcb668d60d45c6530248
Avanish <avanishtiwari38@gmail.com>
2019-07-12 18:20:56 +0530

    test

--- app/handlers/test.py
+++ app/handlers/test.py
@@ -1,11 +1,11 @@
 import logging
 
 from flask_restful import Resource
-from flask import request
+from flask import request, jsonify
 
 logger = logging.getLogger(__name__)
 
 class Hello(Resource):
 	"""docstring for Hello"""
 	def get(self):
-		return "Hello"
+		return jsonify(msg="It is working", status=200)
